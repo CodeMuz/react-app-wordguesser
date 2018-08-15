e5bf0ccb22d7bddc88061051d3bf2029faf1c3c3
CodeMuz <mrwynnes@gmail.com>
2018-08-15 20:39:30 +0100

    Fixed redux dev tools on production

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,23 @@ import "babel-polyfill";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, compose,createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import App from "./app/App";
 import "./index.css";
 import reducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
 
+// dev tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 /* eslint-disable no-underscore-dangle */
-export const store = createStore(reducer, 
-  compose(applyMiddleware(thunk),(window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()));
+export const store = createStore(reducer, composeSetup(applyMiddleware(thunk)));
 /* eslint-enable */
 
 ReactDOM.render(
